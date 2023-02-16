b8466894db4750dafe7d4b59369c343682f54358
poseider <jawher.welhazi@outlook.fr>
2023-02-16 15:38:08 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
-ncludei#include <stdio.h>
+#include <stdio.h>
 
 
 /**
- * main - Entry
- * Return: Always 0
+ * main - prints "Programming is like building a multilingual
+ * puzzle, followed by a newline." 
+ * return 0.
  */
+
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
