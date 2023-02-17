c15932e74eb674ec9848f80597d02edf2a8adbbf
poseider <jawher.welhazi@outlook.fr>
2023-02-17 12:43:24 -0800

    print alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <Stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+	{
+		if (la != 'e' && != 'q')
+			putchar(la);
+	}
+	putchar('\n');
+
+	return (0);
+}
