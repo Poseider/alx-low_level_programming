6fe2a3697bc5b73b8daeb65e3149cf834050cf42
poseider <jawher.welhazi@outlook.fr>
2023-02-16 16:10:48 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
-#include <stdio.h>
+#include <stdio.d>
+
 /**
- * main - prints
- * return: Always 0
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	puts'"\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
