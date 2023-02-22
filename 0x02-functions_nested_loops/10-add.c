4ca4a8de084508a11c33a5c22485c3860de6425a
poseider <jawher.welhazi@outlook.fr>
2023-02-21 16:44:08 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * @n1 : first number
+ * @n2 : second number
+ * Return: Always 0 (Success)
+ */
+
+int add(int n1, int n2)
+
+{
+	return (n1 + n2);
+}
